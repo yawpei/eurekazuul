5cf21a4a75e1f5bd0b1f73357989adcb9133a1e7
yaw <peiyawei@lczq.com>
2021-11-24 21:52:10 +0800

    eureka+zuul集群初始化

---
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.example.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Server，注册中心
+ *
+ * @author yaw
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ simpleService/src/main/java/com/example/simpleservice/SimpleServiceApplication.java
@@ -0,0 +1,29 @@
+package com.example.simpleservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+@SpringBootApplication
+public class SimpleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleServiceApplication.class, args);
+    }
+
+    @RestController
+    class SimpleController {
+        @Value("${eureka.instance.metadataMap.zone}")
+        private String zone;
+
+        @GetMapping(value = "/zone", produces = APPLICATION_JSON_UTF8_VALUE)
+        public String zone() {
+            return "{\"zone\"=\"" + zone + "\"}";
+        }
+    }
+}
